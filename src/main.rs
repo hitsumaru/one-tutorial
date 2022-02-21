305740973d751da7cabcb74da9a3a07bb65d64e9
hitsumaru <vmsilva19@gmail.com>
2022-02-21 01:15:22 -0300

    - Add dependency dotenv - Changed request header to get  variable from dotenv with .env file

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    let cmc_pro_api_key = dotenv::var("CMC_PRO_API_KEY").expect("CMC key not set");
+
     let mut params = HashMap::new();
     params.insert("symbol", "BTC");
     let client = reqwest::Client::new();
 
     let resp = client
         .get(" https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest")
-        // this section will be commented with generic header
-        // TODO find a form to use variable environment or something else to catch the api key
-        .header("X-CMC_PRO_API_KEY", "YOUR_API_KEY")
+        .header("X-CMC_PRO_API_KEY", cmc_pro_api_key)
         .query(&params)
         .send()
         .await?;
